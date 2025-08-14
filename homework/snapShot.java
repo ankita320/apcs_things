bce425ff0a25d3e1b9bc6253d6e2d8b275db66a1
Ankita Saha <76600603+ankita320@users.noreply.github.com>
2025-08-14 00:28:44 -0400

    Create snapShot.java

---
+++ homework/snapShot.java
@@ -0,0 +1,10 @@
+public class SnapShot {
+  public static vois main(String[] args) {
+    String[] array = {"Welcome \n","to \n","the snap shot \n","app!"};
+    System.out.println(array);
+    String[0] = "Upa=grade \n";
+    String[3] = "premium app! \n";
+    System.out.println(array);
+      
+  }
+}
