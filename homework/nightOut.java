d8ec0589fc13ace2b8eaedd691ebf33a0c36c6fe
Ankita Saha <76600603+ankita320@users.noreply.github.com>
2025-08-14 00:24:12 -0400

    Create nightOut.java

---
+++ homework/nightOut.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class NightOut {
+    public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
+    
+        System.out.println("Enter your dinner cost:");
+        double dinner = input.nextDouble();
+        input.nextLine();
+
+        System.out.println("Enter your laser tag cost:");
+        double laserTag = input.nextDouble();
+        input.nextLine();
+
+        System.out.println("Enter your icecream cost:");
+        double icecream = input.nextDouble();
+        input.nextLine();
+
+        double friendDinner = 3.4;
+        double friendDinner = 3.4;
+        double friendDinner = 3.4;
+
+
+    }
+}
