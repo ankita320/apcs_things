547d8775303eacdb7fbf6e3a412e188d851f9414
Ankita Saha <76600603+ankita320@users.noreply.github.com>
2025-08-14 00:20:28 -0400

    Create Variables.java

---
+++ homework/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+        String myName = "Ankita";
+        int myAge = 16;
+        String drawing = "cat";
+        System.out.println("My name is " + myName + ".");
+        System.out.println("I am " + myAge + ".");
+        System.out.println("I am drawing a " + drawing + ".");
+        System.out.println(" _/\\_/\\_");
+        System.out.println("| o   o |");
+        System.out.println("|   -   |");
+        System.out.println(" \\_____/");
+    }
+}
