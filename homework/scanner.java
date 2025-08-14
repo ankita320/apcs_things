7cd0ae1258c61ec73901b24d018ba1a602539881
Ankita Saha <76600603+ankita320@users.noreply.github.com>
2025-08-14 00:23:44 -0400

    Create scanner.java

---
+++ homework/scanner.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class MyScanner {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter your age:");
+        int age = input.nextInt();
+        input.nextLine();
+
+        System.out.println("Enter your name:");
+        String name = input.nextLine();
+
+        System.out.println("Hi " + name + "!" );
+        System.out.println("You are " + age + " years old!");
+        
+    }
+}
